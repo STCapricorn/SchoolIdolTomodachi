044073f95d4a62492ae66f94b684bee6a80f3303
LatidoReMe <30684769+LatidoReMe@users.noreply.github.com>
2018-06-22 14:52:32 -0700

    Fixed version and temp location filter

--- sukutomo/forms.py
+++ sukutomo/forms.py
@@ -131,7 +131,13 @@ class SongFilterForm(MagiFiltersForm):
         ('master_difficulty', string_concat('MASTER - ', _('Difficulty'))),
         ('master_notes', string_concat('MASTER - ', _('Notes'))),
     ]
+
+    location = forms.forms.ChoiceField(label=_('Location'), choices=BLANK_CHOICE_DASH + models.Song.LOCATIONS_CHOICES)
+    version_filter = MagiFilter(to_queryset=lambda form, queryset, request, value: queryset.filter(c_locations__contains=value))
+
+    version = forms.forms.ChoiceField(label=_(u'Server availability'), choices=BLANK_CHOICE_DASH + models.Account.VERSION_CHOICES)
+    version_filter = MagiFilter(to_queryset=lambda form, queryset, request, value: queryset.filter(c_versions__contains=value))
     
     class Meta:
         model = models.Song
-        fields = ('search', 'i_attribute', 'i_unit', 'i_subunit')
+        fields = ('search', 'i_attribute', 'i_unit', 'i_subunit', 'location', 'version')
